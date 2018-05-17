99965a26fb5e06b7eba8b6152d1c18329418816e
Lukas Lazarek <LukasALazarek@gmail.com>
2018-05-17 09:56:19 -0400

    model: refactor conditions into side-condition and where

--- ltl-model.rkt
+++ ltl-model.rkt
@@ -11,18 +11,16 @@
      (or ltl)
      (next ltl)
      (until ltl ltl)]
-  [p ??? #| todo: Any predicate: don't know how to encode |#]
+  [p ??? #| todo: how to encode any predicate? |#]
   [seq empty
        (cons seq-el seq)]
-  [seq-el ??? #| todo: Anything: don't know how to encode |#]
+  [seq-el ??? #| todo: how to encode anything? |#]
   [r #t
      #f]
   [ltl-state (state ltl r seq)]) ;; No ctxs because sub-formula
                                  ;; reduction doesn't make sense.
 
 
-;; How to encode consuming elements of a sequence on each step?
-;; -> try having the sequence as part of the state
 (define ltl-red
   (reduction-relation
    ltl-lang
@@ -36,36 +34,39 @@
 
    (--> (state (first p) r (cons seq-el seq))
         (state true #t seq)
-        #:if (p seq-el) ;; todo: is this how to write rule premises?
-                        ;; Also multiple ones below.
+        ;; todo: is this how to write rule premises? Can I use bound
+        ;; variables from the rule like this?
+        (side-condition (p seq-el))
         r-first-true)
    (--> (state (first p) r (cons seq-el seq))
         (state false #f seq)
-        #:if (not (p seq-el))
+        (side-condition (not (p seq-el)))
         r-first-false)
 
    (--> (state (all p) r (cons seq-el seq))
         (state (all p) #t seq)
-        #:if (p seq-el)
+        (side-condition (p seq-el))
         r-all-true)
    (--> (state (all p) r (cons seq-el seq))
         (state false #f seq)
-        #:if (not (p seq-el))
+        (side-condition (not (p seq-el)))
         r-all-false)
 
 
    (--> (state (not ltl_0) r_0 (cons seq-el seq))
         (state (not ltl_1) ,(not r_1) seq)
-        #:if (--> (state ltl_0 r_0 (cons seq-el seq))
-                  (state ltl_1 r_1 seq))
+        ;; todo: I don't think this is right, but I can't find how to
+        ;; do this
+        (where (--> (state ltl_0 r_0 (cons seq-el seq))
+                    (state ltl_1 r_1 seq)))
         r-not)
 
    (--> (state (or ltl_A_0 ltl_B_0) r_0 (cons seq-el seq))
         (state (or ltl_A_1 ltl_B_1) ,(or r_A r_B) seq)
-        #:if (--> (state ltl_A_0 r_0 (cons seq-el seq))
-                  (state ltl_A_1 r_A seq))
-        #:if (--> (state ltl_B_0 r_0 (cons seq-el seq))
-                  (state ltl_B_1 r_B seq))
+        (where (--> (state ltl_A_0 r_0 (cons seq-el seq))
+                    (state ltl_A_1 r_A seq)))
+        (where (--> (state ltl_B_0 r_0 (cons seq-el seq))
+                    (state ltl_B_1 r_B seq)))
         r-or)
 
    (--> (state (next ltl) r (cons seq-el seq))
@@ -74,15 +75,15 @@
 
    (--> (state (until ltl_A_0 ltl_B_0) r (cons seq-el seq))
         (state ltl_B_1 #t seq)
-        #:if (--> (state ltl_B_0 r (cons seq-el seq))
-                  (state ltl_B_1 #t seq))
+        (where (--> (state ltl_B_0 r (cons seq-el seq))
+                    (state ltl_B_1 #t seq)))
         r-until-start-B)
    (--> (state (until ltl_A_0 ltl_B_0) r (cons seq-el seq))
         (state (until ltl_A_1 ltl_B_0) #f seq)
-        #:if (--> (state ltl_B_0 r (cons seq-el seq))
-                  (state ltl_B_1 #f seq))
-        #:if (--> (state ltl_A_0 r (cons seq-el seq))
-                  (state ltl_A_1 r_A seq))
+        (where (--> (state ltl_B_0 r (cons seq-el seq))
+                    (state ltl_B_1 #f seq)))
+        (where (--> (state ltl_A_0 r (cons seq-el seq))
+                    (state ltl_A_1 r_A seq)))
         r-until-still-A)
 
 

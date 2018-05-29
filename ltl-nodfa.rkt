6f445c2fcd2c9ace35118e8b706c368cbfa70e01
Lukas Lazarek <LukasALazarek@gmail.com>
2018-05-29 17:47:08 -0400

    Refactor combinator definitions using define-ltl-combinator

--- ltl-nodfa.rkt
+++ ltl-nodfa.rkt
@@ -7,9 +7,9 @@
   ;; location so that test failure points to the actual test code that
   ;; fails
   #;(define-syntax (check-runs stx)
-    (syntax-case stx (->)
-      [(check-runs ltl seq ... -> accept?)
-       #'(check-match (run ltl '(seq ...)) (ltl-state _ accept? '()))]))
+      (syntax-case stx (->)
+        [(check-runs ltl seq ... -> accept?)
+         #'(check-match (run ltl '(seq ...)) (ltl-state _ accept? '()))]))
   (define-syntax (check-runs stx)
     (syntax-case stx (->)
       [(check-runs ltl seq ... -> accept?)
@@ -24,16 +24,39 @@
                   [pat #t]
                   [_ #f]))))
 
+;; Base definitions
 (define world/c any/c)
 (define-struct ltl ())
 (define-struct (ltl/true ltl) ())
 (define-struct (ltl/false ltl) ())
-(define-struct/contract (ltl/first ltl) ([predicate (-> world/c boolean?)]))
-(define-struct/contract (ltl/all ltl) ([predicate (-> world/c boolean?)]))
-(define-struct/contract (ltl/not ltl) ([ltl ltl?]))
-(define-struct/contract (ltl/or ltl) ([lhs ltl?] [rhs ltl?]))
-(define-struct/contract (ltl/next ltl) ([ltl ltl?]))
-(define-struct/contract (ltl/until ltl) ([first ltl?] [then ltl?]))
+(define-struct/contract (ltl/first ltl) ([predicate predicate/c]))
+(define-struct/contract (ltl/all ltl) ([predicate predicate/c]))
+
+(define (ensure-ltl . xs)
+  (unless (andmap ltl? xs)
+    (error "Error: non-ltl provided to ltl combinator.")))
+
+(define-syntax (define-ltl-combinator stx)
+  (syntax-case stx ()
+    [(define-ltl-combinator name f ...)
+     (syntax/loc stx
+       ;; I'd like to use define-struct/contract here, but it doesn't
+       ;; seem to work
+       (struct name ltl (f ...) #:guard ensure-ltl))]))
+
+;; Combinators
+(define-ltl-combinator ltl/not ltl)
+(define-ltl-combinator ltl/or lhs rhs)
+(define-ltl-combinator ltl/next ltl)
+(define-ltl-combinator ltl/until first then)
+(define-ltl-combinator ltl/and lhs rhs)
+(define-ltl-combinator ltl/implies lhs rhs)
+(define-ltl-combinator ltl/iff lhs rhs)
+(define-ltl-combinator ltl/release releaser held)
+(define-ltl-combinator ltl/eventually ltl)
+(define-ltl-combinator ltl/globally ltl)
+(define-ltl-combinator ltl/weak-until first then)
+(define-ltl-combinator ltl/strong-release releaser held)
 
 (define-struct/contract ltl-state ([ltl ltl?]
                                    [accept? boolean?]
@@ -45,54 +68,54 @@
   (match state
     [(ltl-state _ _ '()) state]
 
-    [(ltl-state (ltl/true) _ (cons h t)) (make-ltl-state (ltl/true) #t t)]
+    [(ltl-state (ltl/true) _ (cons h t)) (ltl-state (ltl/true) #t t)]
 
-    [(ltl-state (ltl/false) _ (cons h t)) (make-ltl-state (ltl/false) #f t)]
+    [(ltl-state (ltl/false) _ (cons h t)) (ltl-state (ltl/false) #f t)]
 
     [(ltl-state (ltl/first p) _ (cons h t))
      (let* ([res (p h)]
             [new-ltl (if res (ltl/true) (ltl/false))])
-       (make-ltl-state new-ltl res t))]
+       (ltl-state new-ltl res t))]
 
     [(ltl-state (ltl/all p) _ (cons h t))
      (let* ([res (p h)]
-            [new-ltl (if res (make-ltl/all p) (ltl/false))])
-       (make-ltl-state new-ltl res t))]
+            [new-ltl (if res (ltl/all p) (ltl/false))])
+       (ltl-state new-ltl res t))]
 
     [(ltl-state (ltl/not ltl) r (cons h t))
      (let* ([stepped-inner (step (ltl-state ltl r (cons h t)))]
-            [new-ltl (make-ltl/not (ltl-state-ltl stepped-inner))]
+            [new-ltl (ltl/not (ltl-state-ltl stepped-inner))]
             [res (not (ltl-state-accept? stepped-inner))])
-       (make-ltl-state new-ltl res t))]
+       (ltl-state new-ltl res t))]
 
     [(ltl-state (ltl/or lhs rhs) r (cons h t))
      (let* ([stepped-lhs (step (ltl-state lhs r (cons h t)))]
             [stepped-rhs (step (ltl-state rhs r (cons h t)))]
-            [new-ltl (make-ltl/or (ltl-state-ltl stepped-lhs)
-                                  (ltl-state-ltl stepped-rhs))]
+            [new-ltl (ltl/or (ltl-state-ltl stepped-lhs)
+                             (ltl-state-ltl stepped-rhs))]
             [res (or (ltl-state-accept? stepped-lhs)
                      (ltl-state-accept? stepped-rhs))])
-       (make-ltl-state new-ltl res t))]
+       (ltl-state new-ltl res t))]
 
     [(ltl-state (ltl/next ltl) r (cons h t))
-     (make-ltl-state ltl #f t)]
+     (ltl-state ltl #f t)]
 
     [(ltl-state (ltl/until first-ltl then-ltl) r (cons h t))
      (let* ([stepped-then (step (ltl-state then-ltl r (cons h t)))]
             [then-accepted? (ltl-state-accept? stepped-then)]
             [stepped-first (step (ltl-state first-ltl r (cons h t)))])
        (if then-accepted?
-           (make-ltl-state (ltl-state-ltl stepped-then) #t t)
-           (make-ltl-state (make-ltl/until (ltl-state-ltl stepped-first)
-                                           then-ltl)
-                           #f ;; this is strong until; then must become true
-                           t)))]))
+           (ltl-state (ltl-state-ltl stepped-then) #t t)
+           (ltl-state (ltl/until (ltl-state-ltl stepped-first)
+                                 then-ltl)
+                      #f ;; this is strong until; then must become true
+                      t)))]))
 
 
 (define/contract (run ltl seq [init #f])
   ((ltl? (listof world/c)) (boolean?) . ->* . ltl-state?)
 
-  (step-all (make-ltl-state ltl init seq)))
+  (step-all (ltl-state ltl init seq)))
 
 (define/contract (step-all state)
   (ltl-state? . -> . ltl-state?)

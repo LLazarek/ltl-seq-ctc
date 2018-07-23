22ff950bf6ab1fee469b4c99d691af2b4cd11120
Lukas Lazarek <LukasALazarek@gmail.com>
2018-07-23 15:41:37 -0400

    Fix unparsable ltl operators like X

--- ltl/expander.rkt
+++ ltl/expander.rkt
@@ -17,23 +17,24 @@
   (with-pattern ([(DEFS-DATUM ...) (format-datums '~a def-sexps)])
     #'(begin DEFS-DATUM ...)))
 
-;; (require (for-syntax racket/string))
-;; (define-macro (formula-def FORMULA-STR)
-;;   (define formula-elements (string-split (syntax->datum #'FORMULA-STR)))
-;;   (with-pattern ([(FORMULA-DATUM ...) (format-datums '~a formula-elements)])
-;;     #'(define FORMULA-DATUM ...)))
-
 (define-macro (formula-def "[" ID BODY ... "]")
   ;; Using format-id allows the id definition to be seen
+  ;; Outside the macro
   (with-pattern ([ID-STX (format-id #'ID "~a" (syntax-e #'ID))]
-                 [(BODY-STX ...) (format-datum '~a #'(BODY ...))])
-    #'(define ID-STX (ltl-formula BODY-STX ...))))
+                 ;; [(BODY-STX ...) (format-datum '~a #'(BODY ...))]
+                 )
+    #'(define ID-STX (ltl-formula BODY ...))))
 
 (define-macro-cases ltl-formula
   [(ltl-formula "X" FORMULA)
-   #''(c/next FORMULA)]
+   #'(list 'c/next (ltl-formula FORMULA))]
   [(ltl-formula PRED)
-   #''(primitive/first PRED)])
+   (with-pattern ([PRED-ID (format-id #'PRED "~a" (syntax-e #'PRED))])
+     #'(list 'primitive/first 'PRED-ID))]
+  [(ltl-formula A ...)
+   #''(catch-all A ...)])
 
 (provide ltl-definitions racket-defs formula-def)
+(require racket/list)
+(provide (all-from-out racket/list))
 
